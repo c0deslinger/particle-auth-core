ba8bbe94a839c318421922323739e4ee509ec81b
c0deslinger <yusuf.fachroni@gmail.com>
2025-10-01 12:30:57 +0700

    unused ios

---
+++ example/ios/Pods/Target Support Files/particle_auth_core/particle_auth_core-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_particle_auth_core : NSObject
+@end
+@implementation PodsDummy_particle_auth_core
+@end
---
+++ example/ios/Pods/Target Support Files/particle_base/particle_base-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_particle_base : NSObject
+@end
+@implementation PodsDummy_particle_base
+@end
