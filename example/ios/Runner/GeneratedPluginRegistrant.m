52fa49a45be70f153b11b137530de478a91eb0c0
c0deslinger <yusuf.fachroni@gmail.com>
2025-10-01 12:44:11 +0700

    edit particle base

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -12,24 +12,10 @@
 @import integration_test;
 #endif
 
-#if __has_include(<particle_auth_core/ParticleAuthCorePlugin.h>)
-#import <particle_auth_core/ParticleAuthCorePlugin.h>
-#else
-@import particle_auth_core;
-#endif
-
-#if __has_include(<particle_base/ParticleBasePlugin.h>)
-#import <particle_base/ParticleBasePlugin.h>
-#else
-@import particle_base;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [IntegrationTestPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntegrationTestPlugin"]];
-  [ParticleAuthCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"ParticleAuthCorePlugin"]];
-  [ParticleBasePlugin registerWithRegistrar:[registry registrarForPlugin:@"ParticleBasePlugin"]];
 }
 
 @end
